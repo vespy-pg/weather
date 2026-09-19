7f5cadb0a59c11aa1280e5b42029e78ab0c3d5e6
Pawel Gruszka <vespy.pawel.gruszka@gmail.com>
2026-09-19 21:58:04 +0200

    Use compatible Android system bar APIs

--- apps/android/app/src/main/java/eu/vespy/weather/ui/SystemBars.kt
+++ apps/android/app/src/main/java/eu/vespy/weather/ui/SystemBars.kt
@@ -1,20 +1,13 @@
 package eu.vespy.weather.ui
 
-import android.os.Build
-import android.view.View
 import android.view.Window
-import android.view.WindowInsets
-import android.view.WindowInsetsController
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 internal fun hideNavigationControls(window: Window) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-        window.insetsController?.apply {
-            hide(WindowInsets.Type.navigationBars())
-            systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-        }
-    } else {
-        @Suppress("DEPRECATION")
-        window.decorView.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+    WindowCompat.getInsetsController(window, window.decorView).apply {
+        hide(WindowInsetsCompat.Type.navigationBars())
+        systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
     }
 }
